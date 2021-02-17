7468095f84a9c6d4eb5ed7d1209b875658ea5b6a
KlaraHajdu <49884249+KlaraHajdu@users.noreply.github.com>
2021-02-17 10:35:56 +0100

    fix: fix firebase import to optimise for build

--- src/services/FirebaseRD/fbDatabase.ts
+++ src/services/FirebaseRD/fbDatabase.ts
@@ -1,4 +1,5 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
+import 'firebase/database'
 import { firebaseConfig } from "./firebaseConfig";
 import GamePhase from "../../types/GamePhase";
 
